93d89e405e20a7d0f4ae18a9edfabced2e7397fc
Aayush Ghimire <aghimire491@gmail.com>
2024-02-12 19:32:41 +0545

    changes

---
+++ psn-backend/src/main/com/psnbackend/entity/UserEntity.java
@@ -0,0 +1,33 @@
+package com.linhtch90.psnbackend.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "User")
+public class UserEntity {
+    @Id
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    private String role;
+
+    List<String> following = new ArrayList<>();
+
+    List<String> follower = new ArrayList<>();
+}
